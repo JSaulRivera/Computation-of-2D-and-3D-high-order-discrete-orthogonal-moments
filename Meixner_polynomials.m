e7721d4e3ae9479fb53d7692fccff18ea54a8d20
JSaulRivera <71569550+JSaulRivera@users.noreply.github.com>
2021-06-03 08:26:38 -0500

    Update Meixner_polynomials.m

--- Meixner_polynomials.m
+++ Meixner_polynomials.m
@@ -1,44 +1,94 @@
 function M=Meixner_polynomials(n,N,a1,b)
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                                                                              %
+%   Downloaded from                                                            %
+%   https://github.com/JSaulRivera/Computation-of-2D-and-3D-high-order-        %
+%   discrete-orthogonal-moments.git                                            %
+%                                                                              %
+%                                                                              %
+%   This code calculate the discrete orthogonal Meixner polynomial             % 
+%   for high order, using Gram-smith orthogonalization.'                       %
+%                                                                              %
+%                                                                              %
+%                                                                              %
+%   Please cite the following documents as:                                    %
+%                                                                              %
+%  *  JosÃ© S. Rivera-Lopez, CÃ©sar Camacho-Bello, and Lucia                   %
+%     GutiÃ©rrez-Lazcano, Chapter 3: â€œComputation of 2D and 3D High-order    %
+%     Discrete Orthogonal Momentsâ€?. Recent Progress in Image Moments and     %
+%     Moment Invariants, GCSR Volume 7 (2021), 53-74, DOI: 10.15579/gcsr.      %
+%     vol7.ch3.                                                                %
+%                                                                              %
+%        Bibtex:                                                               %
+%        @article{Rivera2021Recent,                                            %
+%        title={Recent Progress in Image Moments and Moment Invariants},       %
+%        author={JosÃ© S. Rivera-Lopez and CÃ©sar Camacho-Bello and Lucia      %
+%        GutiÃ©rrez-Lazcano},                                                  %
+%        journal={Science Gate},                                               %
+%        volume={7},                                                           %
+%        pages={53--74},                                                       %
+%        year={2021},                                                          %
+%        URL={https://sciencegatepub.com/sgp-books/gcsr/gcsr_vol7/},           %
+%        ISBN={2241-9063}                                                      %
+%        }                                                                     %
+%                                                                              %
+%                                                                              %
+%  *  C. Camacho-Bello and J. S. Rivera-Lopez, â€œSome computational aspects   %
+%     of tchebichef moments for higher orders,â€?Pattern Recognition           %
+%     Letters, vol. 112, pp. 332â€“339, 2018.                                  %
+%                                                                              %
+%        Bibtex:                                                               % 
+%        @article{camacho2018some,                                             %
+%        title={Some computational aspects of Tchebichef moments for           %
+%        higher orders},                                                       %
+%        author={Camacho-Bello, C{\'e}sar and Rivera-Lopez, Jos{\'e} S},       %
+%        journal={Pattern Recognition Letters},                                %
+%        volume={112},                                                         %
+%        pages={332--339},                                                     %
+%        year={2018},                                                          %
+%        publisher={Elsevier}                                                  %
+%        }                                                                     %                                                    
+%                                                                              %
+%                                                                              %
+%                                                                              %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 n=n-1;
 
 
-for x=1:N
-    
+for x=1:N   
  M(1,x)= sqrt(Pochhamer(x,b-1)*(((a1^(x-1)))/(factorial(b-1)))*((1-a1)^b));
  M(2,x)= (b+(x-1)-((x-1)/a1))*sqrt(M(1,x)/(b));
 end
 
 
-   h=sqrt(sum(M(1,:).^2))+eps;
-   M(1,:)=M(1,:)./h;
+h=sqrt(sum(M(1,:).^2))+eps;
+M(1,:)=M(1,:)./h;
    
-   h=sqrt(sum(M(2,:).^2))+eps;
-   M2(2,:)=M(2,:);
+h=sqrt(sum(M(2,:).^2))+eps;
+M2(2,:)=M(2,:);
    
-   M(2,:)=M(2,:)-(M2(2,:)*M(1,:)').*M(1,:);  
-   M(2,:)=M(2,:)./h;
+M(2,:)=M(2,:)-(M2(2,:)*M(1,:)').*M(1,:);  
+M(2,:)=M(2,:)./h;
    
  
- q=n;
-  An=sqrt(b);  
+q=n;
+An=sqrt(b);  
+
 for n=2:q
+   
+   An2=sqrt((n)*(b+n-1));
     
+   for x=1:N 
+        
+       BD =- (((x-1)-((x-1)*a1)-n+1-(a1*n)+a1-(b*a1))*sqrt(a1))/a1;
+       M(n+1,x)=(BD.*M(n,x)-An*M(n-1,x))/An2;
 
+   end
     
-    An2=sqrt((n)*(b+n-1));
-      for x=1:N 
-%        
-   BD =- (((x-1)-((x-1)*a1)-n+1-(a1*n)+a1-(b*a1))*sqrt(a1))/a1;
-   M(n+1,x)=(BD.*M(n,x)-An*M(n-1,x))/An2;
-
-      end
-    An=An2;  
-    
-  
-    
-    
-    
-    
+   An=An2;  
     
    h=sqrt(sum(M(n+1,:).^2))+eps;
    M2(n+1,:)=M(n+1,:);
